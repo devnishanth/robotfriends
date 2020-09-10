c5656a5cf895eed25fafe340e2dc76485a5cf0e7
devnishanth <67795990+devnishanth@users.noreply.github.com>
2020-09-10 13:23:43 +0530

    Add files via upload

---
+++ containers/app.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { robots } from '../components/robots';
+import Cardsarray from '../components/Cardsarray';
+import Searchbox from '../components/Searchbox'
+import './app.css'
+import Scroll from '../components/scroll'
+
+class App extends Component {
+	constructor(){
+		super()
+		this.state={
+			robots:[],
+			searchfield: ''
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response=> response.json())
+		.then(users=>this.setState({robots:users}))
+
+	}
+
+	onSearchchange=(event)=>{
+		this.setState({searchfield: event.target.value})
+	}
+
+	render(){	
+		const {robot , searchfield} = this.state;
+		const filteredRobots= robots.filter(robot=>{
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		});
+			if(this.state.robots.length === 0){
+				return <h1>Loading</h1>
+			}
+			else{
+				return (
+					<div className='tc'>
+						<h1 className='f1'>Robofriends</h1>
+						<Searchbox searchChange={this.onSearchchange}/>
+						<Scroll>
+							<Cardsarray robots = {filteredRobots}/>
+						</Scroll>
+					</div>
+
+				);
+			}	
+	}		
+
+}
+
+export default App;
